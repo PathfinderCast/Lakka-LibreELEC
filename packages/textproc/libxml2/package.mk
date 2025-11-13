99c20c6ffbb4a8e9452bb14fcdf096ea5006ace4
pathfindercast <98231844+PathfinderCast@users.noreply.github.com>
2025-11-12 17:10:27 -0700

    fix libxml python installation

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -3,27 +3,24 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxml2"
-PKG_VERSION="2.9.10"
-PKG_SHA256="aafee193ffb8fe0c82d4afef6ef91972cbaf5feea100edc2f262750611b4be1f"
+PKG_VERSION="2.12.6"
+PKG_SHA256="69f08b81d4532d285ceea4cdb017eb2d948cb87c34c1d64248a92be90e84132d"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org"
-PKG_URL="ftp://xmlsoft.org/libxml2/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="zlib:host Python3:host"
+PKG_URL="https://gitlab.gnome.org/GNOME/${PKG_NAME}/-/archive/v${PKG_VERSION}/${PKG_NAME}-v${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_HOST="zlib:host ninja:host Python3:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="The libxml package contains an XML library, which allows you to manipulate XML files."
 
-PKG_CONFIGURE_OPTS_ALL="ac_cv_header_ansidecl_h=no \
-             --enable-static \
-             --enable-shared \
-             --disable-silent-rules \
-             --enable-ipv6 \
-             --without-python \
-             --with-zlib=${TOOLCHAIN} \
-             --without-lzma"
+PKG_CMAKE_OPTS_ALL="-DBUILD_SHARED_LIBS=ON \
+                    -DLIBXML2_WITH_LZMA=OFF \
+                    -DLIBXML2_WITH_TESTS=OFF"
 
-PKG_CONFIGURE_OPTS_HOST="${PKG_CONFIGURE_OPTS_ALL} --with-zlib=${TOOLCHAIN} --with-python"
+PKG_CMAKE_OPTS_HOST="${PKG_CMAKE_OPTS_ALL} \
+                     -DLIBXML2_WITH_PYTHON=ON"
 
-PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_ALL} --with-zlib=${SYSROOT_PREFIX}/usr --with-sysroot=${SYSROOT_PREFIX}"
+PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_ALL} \
+                       -DLIBXML2_WITH_PYTHON=OFF"
 
 post_makeinstall_target() {
   sed -e "s:\(['= ]\)/usr:\\1${SYSROOT_PREFIX}/usr:g" -i ${SYSROOT_PREFIX}/usr/bin/xml2-config
